dd23dfff2732f054430d0954854a66d4fa85eae8
gabem20 <157658484+gabem20@users.noreply.github.com>
2025-07-20 02:02:02 +0000

    update

--- chapter6/own/main.py
+++ chapter6/own/main.py
@@ -51,7 +51,7 @@ def get_db():
     tags=["analytics"],
 )
 async def root():
-    return {"message": "API health check successful"}
+    return {"message": "API health check: Successful"}
 
 @app.get(
     "/v0/players/",
